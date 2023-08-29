fc3070298204da1a3568dd90ab95fdc99ef7dfd2
Adam <adamgjohnson92@gmail.com>
2023-08-29 11:31:08 -0700

    changed text variable in getDb to jate to make the initdb function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,18 +15,22 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
+
+
+
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const textDb = await openDB('text', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = textDb.transaction('text', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('text');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
@@ -34,8 +38,11 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
+
+  //TRY, CATCH?//
+  console.error('getDb not implemented');
   return result;
-console.error('getDb not implemented');
+
 }
 
 initdb();
