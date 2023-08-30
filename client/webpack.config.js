bafbb093bb969e81bf1d1f3026fa5aedd2112e7f
Adam <adamgjohnson92@gmail.com>
2023-08-30 12:53:57 -0700

    Got the web app to work and install. Changed a few destinations in webpack.config, and re-ran npm run build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,12 +21,30 @@ module.exports = () => {
 
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.js',
-        title: 'Webpack Plugin',
+        template: './index.html',
+        title: 'J.A.T.E',
       }),
       new InjectManifest({
-        swSrc: './src-sw.js'
-      })
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: "It's a text editor!",
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
